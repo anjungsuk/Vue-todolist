77d1bc2bc84caf885f028687e0a1cd5616b753c3
anjungsuk <rusarmata@naver.com>
2021-12-08 22:19:42 +0900

    초기화 된거 복구작업

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,53 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app" class="container">
+    <h1 class="text-center">Todo App</h1>
+    <input v-model="todoText" class="w-100 p-2" placeholder="Type todo" @keyup.enter="addTodo"/>
+    <todo v-for="todo in todos" :key="todo.id" :todo="todo" @click-delete = "deleteTodo" @toggle-checkbox="toggleCheckbox"></todo>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  import todo from '@/components/Todo';
 
-#nav {
-  padding: 30px;
-}
+export default {
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  components :{
+    todo,
+  },
+  data(){
+    return{
+      todoText : '',
+      todos : [
+        {id : 1, text : 'buy a car', checked : false},
+        {id : 2, text : 'play game', checked : false},
+      ]
+    }
+  },
+  methods: {
+    addTodo(e){
+      this.todos.push({
+        id : Math.random(),
+        text : e.target.value,
+        checked : false
+      })
+      this.todoText = "";
+    },
+    deleteTodo(id){
+      const index = this.todos.findIndex(todo =>{
+        return todo.id === id;
+      });
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+      this.todos.splice(index, 1);
+    },
+    toggleCheckbox({id, checked})
+    {
+      const index = this.todos.findIndex(todo => {
+        return todo.id === id;
+      })
+
+      this.todos[index].checked = checked;
+    }
+  },
 }
-</style>
+</script>
+
